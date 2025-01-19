7959f6bc3fbb83fa6ee62734400047749a908209
Anurag Kumar Singh <anuragkrsingh3456@gmail.com>
2025-01-19 21:38:08 +0530

    initialization

---
+++ lib/widgets/Expenses_list/expenses_item.dart
@@ -0,0 +1,36 @@
+import 'package:expense_tracker/model/expense.dart';
+import 'package:flutter/material.dart';
+
+class ExpensesItem extends StatelessWidget {
+  const ExpensesItem(this.expenses, {super.key});
+  final Expense expenses;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+        child: Column(
+          children: [
+            Text(expenses.title),
+            Row(
+              children: [
+                Text('\$${expenses.amount.toStringAsFixed(2)}'),
+                const Spacer(),
+                Row(
+                  children: [
+                    Icon(categoryIcons[expenses.category]),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Text(expenses.formattedDate)
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/expenses.dart
@@ -0,0 +1,54 @@
+import 'package:expense_tracker/widgets/Expenses_list/expenses_list.dart';
+import 'package:expense_tracker/model/expense.dart';
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() => _ExpensesState();
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        title: "My COurese",
+        amount: 500,
+        date: DateTime.now(),
+        category: Category.travel),
+    Expense(
+        title: "Youtube",
+        amount: 99,
+        date: DateTime.now(),
+        category: Category.food),
+  ];
+  void _openAddExpenseOverly() {
+    showModalBottomSheet(
+        context: context,
+        builder: (ctx) {
+          return Text("Helo");
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Expense Tracker "),
+        centerTitle: true,
+        actions: [
+          IconButton(
+              onPressed: _openAddExpenseOverly, icon: const Icon(Icons.add))
+        ],
+      ),
+      body: Column(
+        children: [
+          Text("Chart"),
+          Expanded(
+            child: ExpensesList(expenses: _registeredExpenses),
+          ),
+        ],
+      ),
+    );
+  }
+}
