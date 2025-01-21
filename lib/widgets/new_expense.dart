f3341f4f12cd69f471607af8f0a288b7ba066e9f
Anurag Kumar Singh <anuragkrsingh3456@gmail.com>
2025-01-21 23:40:24 +0530

    complete showDateSheetBox

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -8,6 +8,22 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  final _titleController = TextEditingController();
+  final _moneyController = TextEditingController();
+
+  void _presentDatePicker() {
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 1, now.month, now.day);
+    showDatePicker(context: context, firstDate: firstDate, lastDate: now);
+  }
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _moneyController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -15,11 +31,63 @@ class _NewExpenseState extends State<NewExpense> {
       child: Column(
         children: [
           TextField(
+            controller: _titleController,
             maxLength: 50,
             decoration: InputDecoration(
-                label: Text("Title"),
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(50))),
+              label: Text("Title"),
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(50)),
+            ),
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _moneyController,
+                  keyboardType: TextInputType.numberWithOptions(decimal: true),
+                  decoration: InputDecoration(
+                    prefixText: "₹ ",
+                    label: Text("Value"),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Select Date",
+                    ),
+                    IconButton(
+                        onPressed: _presentDatePicker,
+                        icon: Icon(Icons.calendar_month))
+                  ],
+                ),
+              )
+            ],
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  print(_titleController.text);
+                },
+                child: Text("Save"),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text("Cancel"),
+              )
+            ],
           )
         ],
       ),
