9ba17e78811d1d3d6d8f21a59798c642b2c7af0d
Anurag Kumar Singh <anuragkrsingh3456@gmail.com>
2025-01-30 15:29:13 +0530

    done

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -14,20 +14,7 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
-  final List<Expense> _registeredExpenses = [
-    Expense(
-      title: 'Flutter Course',
-      amount: 19.99,
-      date: DateTime.now(),
-      category: Category.work,
-    ),
-    Expense(
-      title: 'Cinema',
-      amount: 15.69,
-      date: DateTime.now(),
-      category: Category.leisure,
-    ),
-  ];
+  final List<Expense> _registeredExpenses = [];
 
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
